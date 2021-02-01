a35293e0e1c043b3236d84b8ef7275f6685ef14b
kzollman <kzollman@andrew.cmu.edu>
2021-02-01 09:11:48 -0700

    Delete BSpace-MultiNode directory

--- BSpace-MultiNode/RemoteScripts/jobtemplate.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --job-name=nlogo-{job}
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-
-echo "JobID: {job}"
-/home/kzollman/NetLogo\ 6.2.0/netlogo-headless.sh --model {model} --setup-file {setup} --table {csvfname} --threads 32
